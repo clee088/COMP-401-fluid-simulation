aeee90a7848a9a89073702752995f3b65ea6fbad
Christopher Lee <chris.l.08@icloud.com>
2024-04-23 11:35:52 -0400

    Speed up and slow down time

--- fluid_simulation/src/time.rs
+++ fluid_simulation/src/time.rs
@@ -6,7 +6,11 @@ impl Plugin for TimePlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(
             Update,
-            (toggle_pause.run_if(input_just_pressed(KeyCode::Space)),),
+            (
+                toggle_pause.run_if(input_just_pressed(KeyCode::Space)),
+                change_time_speed::<1>.run_if(input_just_pressed(KeyCode::Up)),
+                change_time_speed::<-1>.run_if(input_just_pressed(KeyCode::Down)),
+            ),
         );
     }
 }
@@ -17,4 +21,14 @@ fn toggle_pause(mut time: ResMut<Time<Virtual>>) {
     } else {
         time.pause();
     }
+}
+
+/// Update the speed of `Time<Virtual>.` by `DELTA`
+fn change_time_speed<const DELTA: i8>(mut time: ResMut<Time<Virtual>>) {
+    let time_speed = (time.relative_speed() + DELTA as f32)
+        .round()
+        .clamp(0.25, 5.);
+
+    // set the speed of the virtual time to speed it up or slow it down
+    time.set_relative_speed(time_speed);
 }
